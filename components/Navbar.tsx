a0cc5694c59f46ab5d8e14479bb81400d0149c87
sandeep <sandeep.pr3@gmail.com>
2025-10-23 15:44:12 +0530

    Add clerk authentication

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { checkUser } from '@/lib/checkUser'
+
+export default function Navbar() {
+    const user=checkUser()
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
